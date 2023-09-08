6f816f524b974ae0ca392029d5cf485a40339b7a
Abhinav Arora <abhinavarora310@gmail.com>
2023-09-09 00:40:22 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -17,9 +17,7 @@ function Dashboard() {
 
   return (
     <div className="relative flex min-h-[calc(100vh-3.5rem)]">
-      <div className="block sm:hidden md:hidden">
       <Sidebar />
-      </div>
       <div className="h-[calc(100vh-3.5rem)] flex-1 overflow-auto">
         <div className="mx-auto w-11/12 max-w-[1000px] py-10">
           <Outlet />
