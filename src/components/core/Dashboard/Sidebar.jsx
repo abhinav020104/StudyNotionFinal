de2109e81fff7b887fdcbfb80c43d5b0a351780c
Abhinav Arora <abhinavarora310@gmail.com>
2023-09-09 09:48:32 +0530

    Update Sidebar.jsx

--- src/components/core/Dashboard/Sidebar.jsx
+++ src/components/core/Dashboard/Sidebar.jsx
@@ -20,7 +20,7 @@ export default function Sidebar() {
 
   if (profileLoading || authLoading) {
     return (
-      <div className="grid h-[calc(100vh-3.5rem)] min-w-[220px] items-center border-r-[1px] border-r-richblack-700 bg-richblack-800">
+      <div className=" h-[calc(100vh-3.5rem)] min-w-[220px] items-center border-r-[1px] border-r-richblack-700 bg-richblack-800 hidden md:grid lg:grid xl:grid">
         <div className="spinner"></div>
       </div>
     )
