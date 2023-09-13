2ed0b6cb20c2b2624bc76b2152f6456c38e3c845
Abhinav Arora <abhinavarora310@gmail.com>
2023-09-13 22:30:52 +0530

    Update index.jsx

--- src/components/core/Dashboard/AddCourse/index.jsx
+++ src/components/core/Dashboard/AddCourse/index.jsx
@@ -8,7 +8,7 @@ export default function AddCourse() {
           <h1 className="mb-14 text-3xl font-medium text-richblack-5">
             Add Course
           </h1>
-          <div className="overflow-x-hidden w-[95%]">
+          <div className="overflow-x-hidden w-[95%] mx-auto">
             <RenderSteps />
           </div>
         </div>
