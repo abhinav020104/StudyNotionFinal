7073dc488618e8c595d495573698a2e64d934b46
Abhinav Arora <abhinavarora310@gmail.com>
2023-09-13 22:29:22 +0530

    1

--- src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
@@ -158,7 +158,7 @@ export default function CourseInformationForm() {
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="space-y-8 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-6 mr-2"
+      className="space-y-8 rounded-md border-[1px] border-richblack-700 bg-richblack-800 pz-3 py-3"
     >
       {/* Course Title */}
       <div className="flex flex-col space-y-2">
