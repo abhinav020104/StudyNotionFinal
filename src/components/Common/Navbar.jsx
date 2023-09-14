9b1444af7d1197242ce3ccf43a698a65d032cc69
Abhinav Arora <abhinavarora310@gmail.com>
2023-09-14 20:51:57 +0530

    Update Navbar.jsx

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -75,7 +75,7 @@ function Navbar() {
   return (
     <div
       className={`flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700 ${
-        location.pathname !== "/" ? "bg-richblack-800" : ""
+        location.pathname !== "/" ? "bg-richblack-900" : ""
       } transition-all duration-200`}
     >
       <div className="flex w-11/12 max-w-maxContent items-center justify-between">
