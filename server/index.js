7569a063ee079e93610d774811a32ee285552679
Abhinav Arora <abhinavarora310@gmail.com>
2024-07-11 17:27:23 +0530

    index.js updated

--- server/index.js
+++ server/index.js
@@ -25,7 +25,9 @@ database.connect();
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors())
+app.use(cors({
+	origin: ["http://localhost:3000", "https://studynotion.codewithabhinav.online/"]
+}));
 app.use(
 	fileUpload({
 		useTempFiles: true,
